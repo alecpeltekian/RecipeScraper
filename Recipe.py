1b3da1438704b53131847ec09dd039b7ea231ebe
Alex Militchinski <alexmilitchinski2024@u.northwestern.edu>
2023-02-20 16:40:32 -0600

    testing push

--- Recipe.py
+++ Recipe.py
@@ -1,12 +1,13 @@
 from recipe_scrapers import scrape_me
 
 
+
 print("Welcome to The Interactive Cookbook. Provide a valid Allrecipes.com url/"
-      "of recipe of your choice or type 'exit' to cancel. ")
+      "of recipe of your choice or type 'exit' to cancel.  ")
 req_url = (input("Enter recipe URL : "))
 
 scraper = scrape_me(req_url)
 
-print(scraper.ingredients())
-#print(scraper.instructions())
-print(scraper.instructions_list())
+#print(scraper.ingredients())
+print(scraper.instructions())
+#print(scraper.instructions_list())
