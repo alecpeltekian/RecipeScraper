226ff8e4c45cc81e60ff9f86bae99fbf70846fe9
Alec Peltekian <alecpeltekian@Alecs-MBP.attlocal.net>
2023-02-20 20:28:54 -0600

    Prompt user done

---
+++ userinput.py
@@ -0,0 +1,23 @@
+from Recipe import *
+
+def list_navigation(list):
+    index = 0
+    for _ in list:
+        print("Below are the list of instructions to create your choice of food")
+        print("1. Choose next step")
+        print("2. Go back to previous step")
+        print("3. Repeat step")
+        print(f"{list[index]} ({index + 1} of {len(list)})")
+        choice = input("Enter your choice: ")
+        if choice == "1" and index < len(list) - 1:
+            index += 1
+        elif choice == "2" and index > 0:
+            index -= 1
+        elif choice == "3":
+            pass
+        else:
+            print("Invalid input. Please choose 1, 2, or 3.")
+        if index == len(list):
+            break
+
+list_navigation(steps)
