f3c0352e3c3064c9bf9d5ebdcf482e596e71d721
Alex Militchinski <alexmilitchinski2024@u.northwestern.edu>
2023-03-02 13:51:02 -0600

    Ingredient question answering

--- IngredientParse.py
+++ IngredientParse.py
@@ -10,13 +10,18 @@ meat = data["specifications"]["meat"]
 seafood = data["specifications"]["seafood"]
 poultry = data["specifications"]["poultry"]
 spices = data["specifications"]["spices"]
+cookingmedium = data["specifications"]["cookingmedium"]
 preparations = data["specifications"]["preparations"]
 
-ingredients = vegetables + meat + seafood + poultry + spices
+preplist = ['prep', 'prepare', 'preperation', 'prepped', 'prepared', 'pre-prep', 'kind', 'type']
+measurelist = ['how much', 'how many', 'quantity', 'measure', 'measurement']
+deflist = ['what is', "what are"]
+Dict = {}
+    
+
+ingredients = vegetables + meat + seafood + poultry + spices + cookingmedium
 
 def Parse(list):
-    Dict = {}
-    
 
     for i in list:
         doc = nlp(i)
@@ -26,6 +31,7 @@ def Parse(list):
         prep = ""
         name = ""
         prev = ""
+        cat = "Other"
         for token in doc:
             if (token.text in measures):
                 unit += token.text
@@ -33,6 +39,18 @@ def Parse(list):
                 amount += token.text
             if (token.text in ingredients):
                 name += token.text
+                if (token.text in vegetables):
+                    cat = "Vegetable"
+                if (token.text in meat):
+                    cat = "Meat"
+                if (token.text in seafood):
+                    cat = "Seafood"
+                if (token.text in poultry):
+                    cat = "Poultry"
+                if (token.text in spices):
+                    cat = "Spice"
+                if (token.text in cookingmedium):
+                    cat = "Cooking Medium"
             if (token.text in preparations):
                 prep += token.text
             combined = prev + " " + token.text
@@ -40,11 +58,57 @@ def Parse(list):
                 name = combined
             if (combined in measures):
                 unit = combined
+            if (combined in preparations):
+                prep = combined
             prev = token.text
-        temp = {"Quantity" : amount, "Unit" : unit, "Preperation" : prep}
+        temp = {"Quantity" : amount, "Unit" : unit, "Preperation" : prep, "Category" : cat}
         Dict[name] = temp
     return Dict
-test = Parse(['1.5 pounds salmon fillets', 'lemon pepper to taste', 'garlic powder to taste', 'salt to taste', '0.33333334326744 cup soy sauce', '0.33333334326744 cup brown sugar', '0.33333334326744 cup water', '0.25 cup vegetable oil'])
 
-print(test)
+
+def IngredientQuestion(q):
+    q = q.lower()
+    doc = nlp(q)
+    ingredient = ''
+    prev = ""
+    for token in doc:
+        if (token.text in ingredients):
+            ingredient = token.text
+        combined = prev + " " + token.text
+        if (combined in ingredients):
+            ingredient = combined
+        prev = token.text
+
+    if(ingredient not in Dict.keys()):
+        print("Not a valid ingredient, try again")
+        return
+    if any(p in q for p in preplist):
+        if(Dict[ingredient]["Preperation"] == ""):
+            string = ingredient + " does not require any prep before starting the recipe"
+            print(string)
+            return
+        else:
+            print(Dict[ingredient]["Preperation"])
+            return
+    if any(m in q for m in measurelist):
+        string = Dict[ingredient]["Quantity"] + ' ' + Dict[ingredient]["Unit"]
+        print(string)
+        return
+    if any(d in q for d in deflist):
+        string = Dict[ingredient]["Category"] + ", look at https://en.wikipedia.org/wiki/" + ingredient.replace(" ", "_") + " for more information"
+        print(string)
+        return  
+    else:
+        print("Not a valid ingredient question, try rephrasing")
+        return
+
+
+
+
+
+test = Parse(['1.5 pounds salmon fillets', 'lemon pepper to taste', 'garlic powder to taste', 'salt to taste', '0.33333334326744 cup soy sauce', '0.33333334326744 cup brown sugar', '0.33333334326744 cup water', '0.25 cup vegetable oil'])
+#print(test)
+#print("onions" in Dict.keys())
+IngredientQuestion("What is salmon")
+#print(cookingmedium)
 
